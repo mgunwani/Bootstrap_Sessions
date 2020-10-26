3964e320e8eb5781ef064ab38424e2ecd7940df7
mgunwani <gunwani.madhu@gmail.com>
2020-10-27 01:26:43 +0530

    Bootstrap Sessions

---
+++ Working_with_Bootstrap/form-validation-example/myscript.js
@@ -0,0 +1,58 @@
+
+function printError(element, msg) {
+    document.getElementById(element).innerHTML = msg;
+}
+
+
+function validateForm() {
+
+    // Retrieving the values of form elements 
+    var name = document.contactForm.name.value;
+    var email = document.contactForm.email.value;
+    var mobile = document.contactForm.mobile.value;
+    var country = document.contactForm.country.value;
+    var gender = document.contactForm.gender.value;
+    var hobbies = [];
+    var checkboxes = document.getElementsByName("hobbies[]");
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            // Populate hobbies array with selected values
+            hobbies.push(checkboxes[i].value);
+        }
+    }
+
+    var nameErr = emailErr = true;
+
+    // Validate name
+    if (name == "") {
+        printError("nameErr", "Please enter your name");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;
+        if (regex.test(name) === false) {
+            printError("nameErr", "Please enter a valid name");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+        }
+    }
+
+    // Validate email address
+    if (email == "") {
+        printError("emailErr", "Please enter your email address");
+    } else {
+        // Regular expression for basic email validation
+        var regex = /^\S+@\S+\.\S+$/;
+        if (regex.test(email) === false) {
+            printError("emailErr", "Please enter a valid email address");
+        } else {
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
+
+    if (nameErr || emailErr == true) {
+        return false;
+    } else {
+        alert('You have submitted the form..')
+    }
+}
