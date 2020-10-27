ffee775c4f4ef13079fc521f6690e8d9d2a5d54a
mgunwani <gunwani.madhu@gmail.com>
2020-10-28 00:47:26 +0530

    Boostrap Sessions Modified:

--- Working_with_Bootstrap/form-validation-example/myscript.js
+++ Working_with_Bootstrap/form-validation-example/myscript.js
@@ -1,58 +1,94 @@
+//something.js
 
-function printError(element, msg) {
-    document.getElementById(element).innerHTML = msg;
+function printError(id, message) {
+    var name = document.getElementById(id);
+    name.innerHTML = message;
+}
+
+function validateField(field, fieldErr, regex) {
+    var f = document.getElementsByName(field);
+    //console.log(field);
+
+    if (f[0].value === "") {
+        printError(fieldErr, "Error: Empty " + field + ".");
+        //alert("false");
+        return false;
+    }
+    else if (regex.test(f[0].value) == false) {
+        printError(fieldErr, "Error: Invalid " + field + ".");
+        //alert("false");
+        return false;
+    }
+    else {
+        printError(fieldErr, "");
+        //alert("true");
+        return true;
+    }
 }
 
 
+//Backend Functions
+function validateName() {
+    var regex = /^[a-zA-Z/s]+$/;
+    return validateField("name", "nameErr", regex);
+}
+
+function validateEmail() {
+    var regex = /^\S+@\S+\.\S+$/;
+    return validateField("email", "emailErr", regex);
+}
+
+function validatePhone() {
+    var regex = /[0-9]{10}/;
+    return validateField("mobile", "mobileErr", regex);
+}
+
+function validateCountry() {
+    var field = document.getElementsByName("country");
+
+    if (field[0].value == "Select") {
+        printError("countryErr", "Error: Invalid country.");
+        return false;
+    }
+    else {
+        printError("countryErr", "");
+        return true;
+    }
+}
+
+function validateGender() {
+    var field = document.getElementsByName("gender");
+
+    if (field[0].value === "") {
+        printError("genderErr", "Error: Gender not chosen.");
+        return false;
+    }
+    else {
+        printError("genderErr", "");
+        return true;
+    }
+}
+
+//Actual function being called
 function validateForm() {
 
-    // Retrieving the values of form elements 
-    var name = document.contactForm.name.value;
-    var email = document.contactForm.email.value;
-    var mobile = document.contactForm.mobile.value;
-    var country = document.contactForm.country.value;
-    var gender = document.contactForm.gender.value;
-    var hobbies = [];
-    var checkboxes = document.getElementsByName("hobbies[]");
-    for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            // Populate hobbies array with selected values
-            hobbies.push(checkboxes[i].value);
+    var submit = document.getElementById("submit");
+    submit.disabled = true;
+
+    //Validation array
+    var valArray = new Array(5);
+    valArray[0] = validateName();
+    valArray[1] = validateEmail();
+    valArray[2] = validatePhone();
+    valArray[3] = validateCountry();
+    valArray[4] = validateGender();
+
+    for (var i = 0; i < valArray.length; i++) {
+        if (valArray[i] == false) {
+            return false;
         }
     }
 
-    var nameErr = emailErr = true;
-
-    // Validate name
-    if (name == "") {
-        printError("nameErr", "Please enter your name");
-    } else {
-        var regex = /^[a-zA-Z\s]+$/;
-        if (regex.test(name) === false) {
-            printError("nameErr", "Please enter a valid name");
-        } else {
-            printError("nameErr", "");
-            nameErr = false;
-        }
-    }
-
-    // Validate email address
-    if (email == "") {
-        printError("emailErr", "Please enter your email address");
-    } else {
-        // Regular expression for basic email validation
-        var regex = /^\S+@\S+\.\S+$/;
-        if (regex.test(email) === false) {
-            printError("emailErr", "Please enter a valid email address");
-        } else {
-            printError("emailErr", "");
-            emailErr = false;
-        }
-    }
-
-    if (nameErr || emailErr == true) {
-        return false;
-    } else {
-        alert('You have submitted the form..')
-    }
+    submit.disabled = false;
+    return true; //if all conditions are successful
 }
